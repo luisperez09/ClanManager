575c3787be41c36d3e165391a98502c5010378dc
Luis Perez <lperez.1092@gmail.com>
2016-09-16 00:12:45 -0430

    Corrige esquema de la base de datos

--- config/schema/schema.sql
+++ config/schema/schema.sql
@@ -2,7 +2,7 @@
 -- Table `clash_db`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `clash_db`.`users` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(25) NOT NULL,
   `email` VARCHAR(45) NULL,
   `password` VARCHAR(45) NULL,
@@ -16,16 +16,10 @@ ENGINE = InnoDB;
 -- Table `clash_db`.`sanciones`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `clash_db`.`sanciones` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `description` TEXT NULL,
   `duration` INT NULL,
   `created` DATE NULL,
   `users_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_sanciones_users_idx` (`users_id` ASC),
-  CONSTRAINT `fk_sanciones_users`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `clash_db`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`)
 ENGINE = InnoDB;
